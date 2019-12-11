a304cf8bb361754433438f90f8f9f178bcaf1b7c
Kyle Tucker <kyleatucker@gmail.com>
2019-12-11 14:10:53 -0500

    Stub remaining API and test suites

---
+++ src/log.js
@@ -0,0 +1,7 @@
+const getEntries = () => {}
+
+const generateYaml = (entries, config) => {}
+
+const writeSourceFile = (content, config) => {}
+
+module.exports = { getEntries, generateYaml, writeSourceFile }
---
+++ src/ship.js
@@ -0,0 +1,7 @@
+const generateMarkdown = (version, config) => {}
+
+const updateChangelog = (content, config) => {}
+
+const bumpVersion = (version, config) => {}
+
+module.exports = { generateMarkdown, updateChangelog, bumpVersion }
