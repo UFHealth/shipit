6907df922adbfd065706564278975f7028fd5cdf
Kyle Tucker <kyleatucker@gmail.com>
2019-12-13 14:08:18 -0500

    Stub full shipit implementation

--- bin/shipit.js
+++ bin/shipit.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
 
-const { run, UsageError } = require('../lib/cli-helpers')
+const {
+  Logger,
+  run,
+  UsageError,
+} = require('../lib/cli-helpers')
+
+const {
+  bumpVersion,
+  clearSources,
+  generateMarkdown,
+  getCurrentVersion,
+  getLoggedChanges,
+  updateChangelog,
+} = require('../lib/api')
 
 // ---------------------------------------------------------------------------------------------
 
@@ -17,14 +30,55 @@ const helpText = `
   Options:
     --help     Display this help message.
     --version  Print the current shipit version.
+    --dry-run  See what would happen if you ran this command IRL.
 `
 
-const main = async (argv) => {
+const main = async (argv, config) => {
   // Grab version
   const version = argv._[0] || false
   if (!version) {
     throw new UsageError('...y u no give version?')
   }
+
+  const logger = new Logger(argv)
+  const debug = argv.debug || false
+  const dryRun = argv['dry-run'] || false
+
+  // Extract changelog data
+  const loggedChanges = await getLoggedChanges(config)
+  logger.debug(`Changes:\n\n`)
+    .inspect(loggedChanges, true)
+
+  // Convert to Markdown
+  const changelogContent = await generateMarkdown(loggedChanges, version)
+  logger.debug(`Generated Markdown:\n\n${changelogContent}`)
+
+  // Write to CHANGELOG.md
+  if (!dryRun) {
+    const changelogUpdated = await updateChangelog(changelogContent, config)
+  } else {
+    const changelogUpdated = false
+  }
+
+  logger.success(`Updated ${chalk.magenta(config.destination)}`)
+
+  // If successful, remove changelog source files
+  if (changelogUpdated) {
+    await clearSources(config)
+  }
+
+  // Find current package version
+  const currentVersion = await getCurrentVersion()
+  logger.debug(`Bumping version from ${currentVersion} -> ${version}...`)
+
+  // Run version bump replacements
+  await Promise.all(Object.keys(config.bump).map(async (path) => {
+    const replacement = config.bump[path]
+    if (!dryRun) {
+      await bumpVersion(path, config.bump[path], currentVersion, version)
+    }
+    logger.success(`Bumped version in ${chalk.magenta(path)}`)
+  }))
 
   return 0
 }
