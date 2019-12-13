ab2af341edf243672fad67a05e60ffeed4574d05
Kyle Tucker <kyleatucker@gmail.com>
2019-12-13 13:00:02 -0500

    Rewrite command structure

--- bin/shipit.js
+++ bin/shipit.js
@@ -1,45 +1,32 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
-const minimist = require('minimist')
 
-const getConfig = require('../src/config')
-const { loadSources, generateMarkdown, updateChangelog } = require('../src/ship')
-const { error, usage, version } = require('../src/cli')
+const { run, UsageError } = require('../lib/cli-helpers')
 
-const argv = minimist(process.argv.slice(2), {
-  alias: {
-    'bump-files': ['b', 'bumpFiles']
-  }
-})
+// ---------------------------------------------------------------------------------------------
 
-if (argv.help) {
-  process.stderr.write(usage())
-  process.exit(0)
-} else if (argv.version) {
-  process.stdout.write(version())
-  process.exit(0)
-}
+const helpText = `
+  Compile changelog entries, write them to CHANGELOG.md, and bump version strings.
+
+  Usage:
+    $ shipit <version> [options]
+
+  Arguments:
+    version  The next version of your package.
+
+  Options:
+    --help     Display this help message.
+    --version  Print the current shipit version.
+`
 
 const main = async (argv) => {
-  try {
-    if (!argv._.length) {
-      process.stderr.write(usage())
-      process.exit(1)
-    }
-
-    const version = argv._[0]
-
-    // Resolve config
-    const config = await getConfig(argv)
-
-    // Get changelog source data
-    const data = await loadSources(config)
-    const markdown = generateMarkdown(version, data)
-    await updateChangelog(version, markdown, config)
-    console.log(`wrote ${chalk.green(markdown.split('\n').length)} lines to ${chalk.cyan(config.destination)}`)
-  } catch (err) {
-    error(err)
+  // Grab version
+  const version = argv._[0] || false
+  if (!version) {
+    throw new UsageError('...y u no give version?')
   }
+
+  return 0
 }
 
-main(argv)
+run(main, helpText)
