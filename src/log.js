79f9a4e41e1a96c4875142a95a34d161334c5d7d
Kyle Tucker <kyleatucker@gmail.com>
2019-12-13 16:49:17 -0500

    Remove first-pass files

--- src/log.js
+++
@@ -1,7 +0,0 @@
-const getEntries = () => {}
-
-const generateYaml = (entries, config) => {}
-
-const writeSourceFile = (content, config) => {}
-
-module.exports = { getEntries, generateYaml, writeSourceFile }
